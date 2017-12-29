b44b4875d906f3d9924c36d0402beadd7c6da9f0
Grzegorz Kalarus <kalarus.g@gmail.com>
2017-12-29 23:30:55 +0100

    Gallery - finished

--- 2_Gallery/js/app.js
+++ 2_Gallery/js/app.js
@@ -8,3 +8,33 @@
    <button class="close">Close</button>
  </div>
  */
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var allImgs = document.querySelectorAll('img');
+    var body = document.querySelector('body');
+
+    console.log(allImgs);
+    console.log(body);
+
+    allImgs.forEach(function (img) {
+        img.addEventListener('click', function (event) {
+            var src = this.getAttribute('src');
+            var newDiv = document.createElement('div');
+            var newImg = document.createElement('img');
+            var newBtn = document.createElement('button');
+
+            body.appendChild(newDiv).appendChild(newImg);
+            newDiv.appendChild(newBtn);
+            newBtn.innerText = "Close";
+
+            newDiv.classList.add('fullScreen');
+            newImg.setAttribute('src', src);
+            newBtn.classList.add('close');
+
+            newBtn.addEventListener('click', function (event) {
+                newDiv.parentElement.removeChild(newDiv);
+            });
+        });
+    });
+});
