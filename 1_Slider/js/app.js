7b873f403b29e0340e79ceddd94cfb831c254eaf
Grzegorz Kalarus <kalarus.g@gmail.com>
2017-12-29 23:23:35 +0100

    slider - finished

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -1,3 +1,33 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function() {
+    var prevBtn = document.querySelector('#prevPicture');
+    var nextBtn = document.querySelector('#nextPicture');
+    var list = document.querySelector('div');
+    var index = 0;
+    var lis = document.querySelectorAll('li');
+
+    list.classList.add('slider');
+
+    lis[index].classList.add('visible');
+
+    prevBtn.addEventListener('click', function (event) {
+        lis[index].classList.remove('visible');
+        index--;
+        if (index < 0) {
+            index = lis.length-1;
+        }
+        lis[index].classList.add('visible');
+    });
+
+
+    nextBtn.addEventListener('click', function (event) {
+        lis[index].classList.remove('visible');
+        index++;
+        if (index >= lis.length) {
+            index = 0;
+        }
+        lis[index].classList.add('visible');
+    });
+
+
+
+});
