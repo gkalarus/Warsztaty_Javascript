720b8f323b5b0dab602b1004336f5a4741ec9c09
Grzegorz Kalarus <kalarus.g@gmail.com>
2017-12-29 23:45:14 +0100

    Menu - finished

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -1,3 +1,30 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var menu = document.querySelector('div');
+    menu.classList.add('nav');
+    var menuLis = document.querySelectorAll('div > ul > li');
+
+    menuLis.forEach(function (menuLi) {
+        menuLi.addEventListener('mouseover', function (event) {
+            var subMenu = this.querySelector('ul');
+            if (subMenu) {
+                subMenu.style.display = 'block';
+            }
+
+        });
+
+        menuLi.addEventListener('mouseout', function (event) {
+            var subMenu = this.querySelector('ul');
+            if (subMenu) {
+                subMenu.style.display = 'none';
+            }
+
+        });
+    })
+
+});
+
