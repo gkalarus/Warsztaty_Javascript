3f7cf44b47e9f42eaab6ea7a1175cc2c8fb07b53
Grzegorz Kalarus <kalarus.g@gmail.com>
2017-12-29 23:40:26 +0100

    Gallery With Filter - finished

--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -1,3 +1,40 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+document.addEventListener('DOMContentLoaded', function () {
+    var imgs = document.querySelectorAll('img');
+    var hideButton = document.querySelector('#hideButton');
+    var showButton = document.querySelector('#showButton');
+    var tagInput = document.querySelector('#tagInput');
+
+
+
+    hideButton.addEventListener('click', function (event) {
+        var text = tagInput.innerText;
+        text = tagInput.value;
+
+        imgs.forEach(function (img) {
+            var data = img.dataset.tag;
+
+            if (data.indexOf(text) >= 0) {
+                img.classList.add('invisible');
+            }
+        });
+
+    });
+
+    showButton.addEventListener('click', function (event) {
+        var text = tagInput.innerText;
+        text = tagInput.value;
+
+        imgs.forEach(function (img) {
+            var data = img.dataset.tag;
+
+            if(data.indexOf(text) >= 0) {
+                img.classList.remove('invisible');
+            }
+        });
+    });
+
+});
+
