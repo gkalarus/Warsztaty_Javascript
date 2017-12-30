b86361dea5ca5825cc743838fd5f96b275fec803
Grzegorz Kalarus <kalarus.g@gmail.com>
2017-12-30 11:21:00 +0100

    Tooltips - finished

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -6,3 +6,25 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltips = document.querySelectorAll('.tooltip');
+    var newTooltip = document.createElement('span');
+
+    tooltips.forEach(function (tooltip) {
+        tooltip.addEventListener('mouseover', function (event) {
+            var tooltipText = this.dataset.text;
+
+            this.appendChild(newTooltip);
+            this.firstElementChild.classList.add('tooltipText');
+            this.firstElementChild.innerText = tooltipText;
+
+        });
+
+        tooltip.addEventListener('mouseout', function (event) {
+            newTooltip.parentElement.removeChild(newTooltip);
+        })
+    })
+});
+
